07f8b686a705ad3cadb5e1c06422ffd2de136da9
Ayffi Nawaz <ayffin@gmail.com>
2017-08-14 15:05:30 -0400

    dom album assignment completed

--- scripts/album.js
+++ scripts/album.js
@@ -45,26 +45,52 @@ var albumMarconi = {
     duration: '2:15'
   }]
 };
-
-var createSongRow = function(songNumber, songName, songLength) {
-  var template =
-    '<tr class="album-view-song-item">' + '  <td class="song-item-number">' +
-    songNumber + '</td>' + '  <td class="song-item-title">' + songName +
-    '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' +
-    '</tr>';
-
-  return template;
-
+var albumJayZ = {
+  title: '4:14',
+  artist: 'Jay Z',
+  label: '© 2017 Roc Nation / Jay-Z',
+  year: '2017',
+  albumArtUrl: 'https://lh3.googleusercontent.com/FW8hKo0Cw_gBK0uqO5Z88G8-nR3z6lhY10A1Dh6Ddp0UR6fwtSRSpzx4pLHlxPOzhYg9EB4K3A=w300-rw',
+  songs: [{
+    title: 'Moonligh',
+    duration: '3:55'
+  }, {
+    title: 'Marcy Me',
+    duration: '2:54'
+  }, {
+    title: 'Legacy',
+    duration: '2:57'
+  }, {
+    title: 'Kill Jay Z',
+    duration: '2:58'
+  }, {
+    title: 'Smile',
+    duration: '3:51'
+  }]
 };
 
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var setCurrentAlbum = function(album) {
   var albumTitle = document.getElementsByClassName('album-view-title')[0];
   var albumArtist = document.getElementsByClassName('album-view-artist')[0];
   var albumReleaseInfo = document.getElementsByClassName(
     'album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
+
   var albumSongList = document.getElementsByClassName('album-view-song-list')[
     0];
+
+  var createSongRow = function(songNumber, songName, songLength) {
+    var template =
+      '<tr class="album-view-song-item">' +
+      '  <td class="song-item-number">' +
+      songNumber + '</td>' + '  <td class="song-item-title">' + songName +
+      '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' +
+      '</tr>';
+
+    return template;
+
+  };
+
 
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
@@ -79,5 +105,35 @@ var setCurrentAlbum = function(album) {
 };
 
 window.onload = function() {
-  setCurrentAlbum(albumPicasso);
+
+  // first method using 3 different callback functions
+  function thr() {
+    setCurrentAlbum(albumMarconi);
+    albumImage.removeEventListener("click", thr);
+    albumImage.addEventListener("click", once);
+  }
+
+  function twic() {
+    setCurrentAlbum(albumJayZ);
+    albumImage.removeEventListener("click", twic);
+    albumImage.addEventListener("click", thr);
+  }
+
+  function once() {
+    setCurrentAlbum(albumPicasso);
+    albumImage.removeEventListener("click", once);
+    albumImage.addEventListener("click", twic);
+  }
+  albumImage.addEventListener("click", once);
+
+
+  //secong method using Math.random;
+  //   albumImage.addEventListener("click", myFunction);
+  //
+  //   function myFunction() {
+  //     var myArray = [albumJayZ, albumMarconi, albumPicasso];
+  //     var rand = myArray[Math.floor(Math.random() * myArray.length)];
+  //     console.log(setCurrentAlbum(rand));
+  //   }
+  //
 };
